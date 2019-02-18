b8e98239ed47b1973d75d4bfc9aa5a225d2367b9
Omid <omidiskawous@gmail.com>
2019-02-17 19:46:01 -0600

    footer.php updated

--- wp-content/themes/BackBone/footer.php
+++ wp-content/themes/BackBone/footer.php
@@ -3,14 +3,16 @@
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  */
 ?>
-		<footer>
-			<div class="container">
-				<?php if( have_rows('footer_links', 'option') ) : while ( have_rows('footer_links', 'option') ) : the_row(); ?>
-				<a href="<?php the_sub_field('link_url'); ?>" target="<?php the_sub_field('link_traget'); ?>" title="<?php the_sub_field('link_name'); ?>"><?php the_sub_field('link_name'); ?></a>
-				<?php endwhile; endif; ?>	
-			</div>
-		</footer>
+		
 	</main>
+
+	<footer>
+		<div class="container">
+			<?php if( have_rows('footer_links', 'option') ) : while ( have_rows('footer_links', 'option') ) : the_row(); ?>
+			<a href="<?php the_sub_field('link_url'); ?>" target="<?php the_sub_field('link_traget'); ?>" title="<?php the_sub_field('link_name'); ?>"><?php the_sub_field('link_name'); ?></a>
+			<?php endwhile; endif; ?>	
+		</div>
+	</footer>
 
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
