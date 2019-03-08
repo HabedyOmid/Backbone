3d6ef62f490cde2758afcd41ce667176ac867865
Omid <omidiskawous@gmail.com>
2019-03-07 22:59:03 -0600

    browserSync + img compression added

--- wp-content/themes/BackBone/assets/gulpfile.js
+++ wp-content/themes/BackBone/assets/gulpfile.js
@@ -4,21 +4,26 @@ const sass          = require('gulp-sass');
 const uglify        = require('gulp-uglify');
 const concat        = require('gulp-concat');
 const cleanCSS      = require('gulp-clean-css');
+const imageMin      = require('gulp-imageMin');
+const pngQuint      = require('imageMin-pngquant'); 
 const browserSync   = require('browser-sync').create();
 const autoprefixer  = require('gulp-autoprefixer');
+const jpgRecompress = require('imageMin-jpeg-recompress'); 
 
 
 // Paths
 var paths = {
-    root: {
-        root: '../',
-        node: 'node_modules'
-    },
     src: {
         css: 'css/',
         php: '../**/*.php',
         js:  'js/**/*.js',
         scss:'scss/**/*.scss',
+        img: '../../../uploads/**/*.+(png|jpg|gif|svg)'
+    },
+    dist: {
+        root:'../',
+        css: '../*.css',
+        img: '../../../uploads/'
     }
 }
 
@@ -26,50 +31,60 @@ var paths = {
 // Compile SCSS
 gulp.task('sass', function() {
     return gulp.src(paths.src.scss)
-
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(paths.src.css))
-    .pipe(browserSync.stream());
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(paths.src.css))
 });
 
 
 // Minify + AutoPreFixer + Combine CSS
 gulp.task('css', function() {
     return gulp.src(paths.src.css + '/*.css')
-    .pipe(autoprefixer({
-        browsers: ['last 10 versions'],
-        cascade: false
-    }))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(concat('style.css'))
-    .pipe(gulp.dest(paths.root.root))
+        .pipe(autoprefixer({
+            browsers: ['last 10 versions'],
+            cascade: false
+        }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest(paths.dist.root))
 });
 
 
 // Minify + Combine JS
 gulp.task('js', function() {
     return gulp.src(paths.src.js)
-    .pipe(uglify())
-    .pipe(concat('backbone.js'))
-    .pipe(gulp.dest(paths.root.root))
-    .pipe(browserSync.stream());
+        .pipe(uglify())
+        .pipe(concat('backbone.js'))
+        .pipe(gulp.dest(paths.dist.root))
+});
+
+
+// Compress (JPEG, PNG, GIF, SVG)
+gulp.task('img', function(){
+    return gulp.src(paths.src.img)
+    .pipe(imageMin([
+        imageMin.gifsicle(),
+        imageMin.jpegtran(),
+        imageMin.optipng(),
+        imageMin.svgo(),
+        pngQuint(),
+        jpgRecompress()
+    ]))
+    .pipe(gulp.dest(paths.dist.img));
 });
 
 
 // Prepare all assets for production
-gulp.task('build', gulp.series('sass', 'css', 'js'));
+gulp.task('build', gulp.series('sass', 'css', 'js', 'img'));
 
 
 // Watch (SASS, CSS, JS, and HTML) reload browser on change
 gulp.task('watch', function() {
     browserSync.init({
-        server: {
-            baseDir: paths.root
-        } 
-    })
-
-    gulp.watch(paths.src.scss, gulp.series('sass'));
-    gulp.watch(paths.src.css, gulp.series('css'));
-    gulp.watch(paths.src.js, gulp.series('js'));
+        proxy: "http://localhost:8888",
+        notify: true
+    });
+    gulp.watch(paths.src.scss, gulp.series('sass')).on('change', browserSync.reload);
+    gulp.watch(paths.src.css, gulp.series('css')).on('change', browserSync.reload);
+    gulp.watch(paths.src.js, gulp.series('js')).on('change', browserSync.reload);
     gulp.watch(paths.src.php).on('change', browserSync.reload);
 });
