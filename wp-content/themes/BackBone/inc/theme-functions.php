c74bccaa9db71c8a7f209c939944fc38f1ef554c
habedyomid <omidiskawous@gmail.com>
2019-08-25 19:06:01 -0500

    theme function.php refactored

--- wp-content/themes/BackBone/inc/theme-functions.php
+++ wp-content/themes/BackBone/inc/theme-functions.php
@@ -131,16 +131,10 @@ add_filter( 'wp_default_scripts', 'dequeue_jquery_migrate' );
 if( function_exists('acf_add_options_page') ) {
     acf_add_options_page(array(
         'page_title'    => 'Theme General Settings',
-        'menu_title'    => 'Theme Settings',
+        'menu_title'    => 'Site Options',
         'menu_slug'     => 'theme-general-settings',
         'capability'    => 'edit_posts',
         'redirect'      => false
-    ));
-    
-    acf_add_options_sub_page(array(
-        'page_title'    => 'Theme Footer Settings',
-        'menu_title'    => 'Footer',
-        'parent_slug'   => 'theme-general-settings',
     ));
 }
 
