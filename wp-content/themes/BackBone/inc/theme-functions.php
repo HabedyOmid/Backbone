4ab7e56df9d36441ac5daab0bf984670d015da78
habedyomid <omidiskawous@gmail.com>
2019-09-15 16:56:03 -0500

    svg support added

--- wp-content/themes/BackBone/inc/theme-functions.php
+++ wp-content/themes/BackBone/inc/theme-functions.php
@@ -105,16 +105,6 @@ function advance_numeric_pagination( $custom_query ) {
 
 
 //
-// Add SVG support
-//
-function cc_mime_types($mimes) {
-    $mimes['svg'] = 'image/svg+xml';
-    return $mimes;
-}
-add_filter('upload_mimes', 'cc_mime_types');
-
-
-//
 // Remove jQuery Migrate File - Front-end ONLY
 // 
 function dequeue_jquery_migrate( &$scripts){
