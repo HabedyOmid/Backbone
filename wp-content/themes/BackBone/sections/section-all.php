1c555b9a7296b5290eb8d1721aee310401f6b57e
habedyomid <omidiskawous@gmail.com>
2019-09-15 15:41:10 -0500

    clean junks, refactored SASS structures

--- wp-content/themes/BackBone/sections/section-all.php
+++ wp-content/themes/BackBone/sections/section-all.php
@@ -1,7 +1,7 @@
 <?php if(have_rows('sections')) : while(have_rows('sections')) : the_row();
 
     if( get_row_layout() == 'wysiwyg' && get_sub_field('visibility') == TRUE ) : // wysiwyg 
-        get_template_part("sections/acf", "wysiwyg");
+        get_template_part("sections/section", "wysiwyg");
 
      endif; //END of all ACFs Sections 
 endwhile; endif; ?>
