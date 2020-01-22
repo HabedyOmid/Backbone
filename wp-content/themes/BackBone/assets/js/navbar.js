df1333e3c777580f453fb9b17805d128e728b37f
habedyomid <omidiskawous@gmail.com>
2020-01-22 15:37:17 -0600

    assets are updated

--- wp-content/themes/BackBone/assets/js/navbar.js
+++ wp-content/themes/BackBone/assets/js/navbar.js
@@ -1,44 +1,34 @@
-$(document).ready(function(){
-    // Check if a dropdown has another dropdown
-    $( "#menu-primary > li ul" ).each(function( index ) {
-        $( this ).has( "ul" ).addClass('parent__dropdown');
-    });
-
-    // Check if <li> has .active class then added to parent <li> for styling
-    $( "#menu-primary li ul li" ).each(function( index ) {
-        if($(this).hasClass("active")){
-            // add .active to parent <li>
-            $(this).parents('ul li').addClass('active');
-            // add .active children <a>
-            $(this).children('a').addClass('active');
-        }
-    });
-
-    // Navbar Toggle
-    $(".navbar__toggler").click(function() {
-        $('#menu-primary').toggleClass('show')
-    })
+// tgl checkbox for navbar collapse
+$('#tgl-checkbox:checkbox').change(function() {
+	$(this).hide();
+	if ($(this).is(':checked')) {
+		$('.navbar-item.links').addClass('show');
+	} else {
+		$('.navbar-item.links').removeClass('show');
+	}
 });
 
+// Hide & Show navbar on scroll
+$(document).ready(function() {
+	'use strict';
+	var c,
+		currentScrollTop = 0,
+		navbar = $('.hideShowOnScroll');
 
+	$(window).scroll(function() {
+		console.log('working');
 
-// Hide & Show on Scroll
-$(document).ready(function () {
-  'use strict';
-   var c, currentScrollTop = 0,
-       navbar = $('.hideShowOnScroll');
+		var a = $(window).scrollTop();
+		var b = navbar.height();
 
-   $(window).scroll(function () {
-      var a = $(window).scrollTop();
-      var b = navbar.height();
-     
-      currentScrollTop = a;
-     
-      if (c < currentScrollTop && a > b + b) {
-        navbar.addClass("scrollUp");
-      } else if (c > currentScrollTop && !(a <= b)) {
-        navbar.removeClass("scrollUp");
-      }
-      c = currentScrollTop;
-  });
+		currentScrollTop = a;
+
+		if (c < currentScrollTop && a > b + b) {
+			navbar.addClass('scrollUp');
+		} else if (c > currentScrollTop && !(a <= b)) {
+			navbar.removeClass('scrollUp');
+		}
+
+		c = currentScrollTop;
+	});
 });
---
+++ wp-content/themes/BackBone/assets/js/zapp.js
@@ -0,0 +1,47 @@
+// Smooth Scrolling
+// $(document).ready(function() {
+// 	$('a').on('click', function(event) {
+// 		if (this.hash !== '') {
+// 			event.preventDefault();
+
+// 			var hash = this.hash;
+
+// 			$('html, body').animate(
+// 				{
+// 					scrollTop: $(hash).offset().top
+// 				},
+// 				700,
+// 				function() {
+// 					window.location.hash = hash;
+// 				}
+// 			);
+// 		}
+// 	});
+// });
+
+// tgl checkbox for navbar collapse
+$('#tgl-checkbox:checkbox').change(function() {
+	if ($(this).is(':checked')) {
+		$('.navbar-item.links').addClass('show');
+	} else {
+		$('.navbar-item.links').removeClass('show');
+	}
+});
+
+// dropdown menu toggle in medium screen size
+$('.dropdown .nav-link').click(function() {
+	$(this).parent().toggleClass('open');
+});
+
+// Boostrap Carousel
+$('.carousel').carousel({
+	interval: 5000
+});
+
+// Animate as Scroll - INIT
+AOS.init();
+
+// Accordion Init
+$('#accordion').collapse({
+	toggle: false
+});
