5f215ca56474c04b6eebb278a77bb25b89a35d06
fluffy <fluffy@fluffys-MacBook-Pro.local>
2020-02-06 19:05:53 -0600

    favicon added

--- wp-content/themes/BackBone/header.php
+++ wp-content/themes/BackBone/header.php
@@ -4,6 +4,12 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri();?>/favicon.png">
+	<link rel="manifest" href="<?php echo get_template_directory_uri();?>/site.webmanifest">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
+
     <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500&display=swap" rel="stylesheet">
 	
 	<?php wp_head(); ?>
