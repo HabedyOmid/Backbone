7a3ac36f1a0f716436a99f05ea6d49ef590d258b
Fluffy <fluffy@Fluffys-iMac.local>
2020-06-01 10:16:07 -0500

    wp core updates

--- wp-content/themes/BackBone/header.php
+++ wp-content/themes/BackBone/header.php
@@ -2,22 +2,25 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="profile" href="https://gmpg.org/xfn/11">
-	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri();?>/favicon.png">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php // FavIcon Generator: https://realfavicongenerator.net/ ?>
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri();?>/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri();?>/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri();?>/favicon-16x16.png">
 	<link rel="manifest" href="<?php echo get_template_directory_uri();?>/site.webmanifest">
+	<link rel="mask-icon" href="<?php echo get_template_directory_uri();?>/safari-pinned-tab.svg" color="#5bbad5">
 	<meta name="msapplication-TileColor" content="#da532c">
 	<meta name="theme-color" content="#ffffff">
 
-    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500&display=swap" rel="stylesheet">
-	
 	<?php wp_head(); ?>
-
 </head>
 
 <body <?php body_class(); ?> >
 
-	<?php get_template_part( 'components/navigation' ); // Get Navbar ?>
+	<?php wp_body_open(); ?>
+
+	<?php get_template_part( 'components/navbar' ); // Get Navbar ?>
 
 	<main>
