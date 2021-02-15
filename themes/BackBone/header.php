c2355b15903088703e7683379aad1d58ff512f2d
Omid Habedy <omidiskawous@gmail.com>
2021-02-14 18:34:50 -0600

    header.php updated

--- themes/BackBone/header.php
+++ themes/BackBone/header.php
@@ -15,11 +15,6 @@
 	<meta name="theme-color" content="#ffffff">
 
 	<?php wp_head(); ?>
-
-	<script
-  src="https://code.jquery.com/jquery-3.5.1.min.js"
-  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-  crossorigin="anonymous"></script>
 </head>
 
 <body <?php body_class(); ?> >
