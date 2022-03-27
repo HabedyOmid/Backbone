ff5cecce11dbc0c7ddc6b17cd82f743623ef8d52
WebAxios <webaxios@WebAxios-Pro.local>
2022-03-27 17:07:37 -0500

    BS Boostrap dependency removed

--- wp-content/themes/BackBone/blocks/page-header.php
+++ wp-content/themes/BackBone/blocks/page-header.php
@@ -1,39 +1,37 @@
 <?php $block_id = $block['id']; ?>
 
 <section class="bg-img bg-img-top" id="custom-<?php echo $block_id; ?>" <?php if( strtolower(get_field('background_type')) == 'color' && get_field('background_color') ) : echo 'style="background-color:' . get_field('background_color') . ';"'; endif; ?>>
-   
-    <div class="container py-6 py-md-12 py-lg-15">
-        <div class="row">
-            <div class="col-md-6 jsutify-content-start justify-content-md-center">
-                <div class="wysiwyg">
-                    <?php echo get_field('content_editor'); ?>
-                </div>
-            </div>
+  <div class="container py-6 py-md-12 py-lg-15">
+    <div class="row">
+      <div class="col-md-6 jsutify-content-start justify-content-md-center">
+        <div class="wysiwyg">
+          <?php echo get_field('content_editor'); ?>
         </div>
+      </div>
     </div>
-
+  </div>
 </section>
 
 <?php // Responsive background image
 if( strtolower(get_field('background_type')) == 'image' && get_field('background_image') ) : ?>
-    <style>
-        @media screen and (min-width: 1200px) {
-            #custom-<?php echo $block_id; ?>{ 
-                background-position: top center;
-                background-image: url('<?php echo get_image_src(get_field('background_image'), 'lg'); ?>'); 
-            }
-        }
-        @media screen and (max-width: 1200px) {
-            #custom-<?php echo $block_id; ?>{ 
-                background-position: top center;
-                background-image: url('<?php echo get_image_src(get_field('background_image'), 'lg'); ?>'); 
-            }
-        }
-        @media screen and (max-width: 768px) {
-            #custom-<?php echo $block_id; ?>{ 
-                background-image: url('<?php echo get_image_src(get_field('background_image'), 'md'); ?>'); 
-            }
-        }
-    </style>
+  <style>
+    @media screen and (min-width: 1200px) {
+      #custom-<?php echo $block_id; ?>{ 
+        background-position: top center;
+        background-image: url('<?php echo get_image_src(get_field('background_image'), 'lg'); ?>'); 
+      }
+    }
+    @media screen and (max-width: 1200px) {
+      #custom-<?php echo $block_id; ?>{ 
+        background-position: top center;
+        background-image: url('<?php echo get_image_src(get_field('background_image'), 'lg'); ?>'); 
+      }
+    }
+    @media screen and (max-width: 768px) {
+      #custom-<?php echo $block_id; ?>{ 
+        background-image: url('<?php echo get_image_src(get_field('background_image'), 'md'); ?>'); 
+      }
+    }
+  </style>
 <?php endif; ?>
 
--- wp-content/themes/BackBone/components/navbar.php
+++ wp-content/themes/BackBone/components/navbar.php
@@ -22,18 +22,18 @@
 		</div>
 
 		<!-- navbar content  -->
-    	<div class="navbar-content">
-			<?php
-			wp_nav_menu( array(
-				'theme_location'    => 'primary-menu',
-				'depth'             => 3,
-				'container'         => 'div',
-				'container_class'   => 'collapse navbar-collapse',
-				'container_id'      => 'navbar-collapse',
-				'menu_class'        => 'nav navbar-nav',
-				'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-				'walker'            => new WP_Bootstrap_Navwalker(),
-			) ); ?>
+    <div class="navbar-content">
+    <?php
+    wp_nav_menu( array(
+      'theme_location'    => 'primary-menu',
+      'depth'             => 3,
+      'container'         => 'div',
+      'container_class'   => 'collapse navbar-collapse',
+      'container_id'      => 'navbar-collapse',
+      'menu_class'        => 'nav navbar-nav',
+      'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+      'walker'            => new WP_Bootstrap_Navwalker(),
+    )); ?>
 		</div>
 	</div>
 </nav>
